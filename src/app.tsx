e225f06a4abbb43ad382fdac0e4e478ba9fcde87
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-02-26 16:24:41 -0300

    feat: the bg color

--- src/app.tsx
+++ src/app.tsx
@@ -1,33 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='min-h-screen bg-gradient-to-b from-gray-900 to-gray-700 flex items-center justify-center p-4'>
+      <div className='max-w-4xl w-full rounded-lg shadow-lg p-8'>
+        <h1 className='text-4xl font-bold text-center mb-6 text-white'>
+          Apogea Wiki
+        </h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
