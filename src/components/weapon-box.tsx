0e3c6aad11495d75352576250d3347d8eec4d410
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-02-26 18:14:23 -0300

    feat: divider some images

--- src/components/weapon-box.tsx
+++ src/components/weapon-box.tsx
@@ -6,11 +6,7 @@ interface WeaponBoxProps {
 export const WeaponBox = ({ title, imageUrl }: WeaponBoxProps) => {
   return (
     <div className='p-4 hover:bg-gray-700/30 cursor-pointer rounded-lg'>
-      <img
-        src={imageUrl}
-        alt={title}
-        className='w-16 h-16 mx-auto mb-2'
-      />
+      <img src={imageUrl} alt={title} className='w-16 h-16 mx-auto mb-2' />
       <p className='text-white text-center'>{title}</p>
     </div>
   )
