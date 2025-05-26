5fcb4e7d9c92330a303fafcb59dd28d3df6e8762
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-05-26 13:09:15 -0300

    feat: update guides types

--- src/@api/guides.d.ts
+++ src/@api/guides.d.ts
@@ -1,13 +1,20 @@
 export as namespace GuidesApiTypes;
 
-export interface Guide {
-  id: string;
-  title: string;
-  description: string;
-  steps: GuideStep[];
+export interface GuideStep {
+  title: string
+  description: string
+  hint?: string
+  item?: string[]
+  note?: string
+  benefit?: string
+  advice?: string
+  image_url?: string
 }
 
-export interface GuideStep {
-  title: string;
-  description: string;
+export interface Guide {
+  id: string
+  title: string
+  description: string
+  steps: GuideStep[]
+  footer_text: string
 }
