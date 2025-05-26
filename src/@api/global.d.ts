25170d0edd870aaa50a45da5ab7ef0f341cb320c
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-05-26 11:14:09 -0300

    feat: add axios

---
+++ src/@api/global.d.ts
@@ -0,0 +1,6 @@
+export as namespace GlobalApiTypes;
+
+export interface RefreshTokenResponse {
+  access: string;
+  refresh: string;
+}
---
+++ src/@api/storage.ts
@@ -0,0 +1,3 @@
+export const USER_TOKEN = "user_token";
+export const USER_TOKEN_REFRESH = "user_token_refresh";
+
