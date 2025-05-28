634d797027a6755ec3d9335e2b9ada753445b618
Talis Lima <147412161+Thelozx@users.noreply.github.com>
2025-05-28 13:45:27 -0300

    feat: ajuste na header: ajuste na other box pra renderizar a pagina other

--- src/components/other-box.tsx
+++ src/components/other-box.tsx
@@ -5,9 +5,9 @@ interface OtherBoxProps {
 
 export const OtherBox = ({ title, imageUrl }: OtherBoxProps) => {
   return (
-    <div className='p-4 hover:bg-gray-700/30 cursor-pointer rounded-lg'>
-      <img src={imageUrl} alt={title} className='w-16 h-16 mx-auto mb-2' />
-      <p className='text-white text-center'>{title}</p>
+    <div className='p-4 hover:bg-gray-700/30 cursor-pointer rounded-lg transition-all duration-300 hover:scale-105'>
+      <img src={imageUrl} alt={title} className='w-16 h-16 mx-auto mb-2 object-contain' />
+      <p className='text-white text-center text-sm font-medium'>{title}</p>
     </div>
   )
 }
