aba8b5d6233f348cfd82e7e9e2498abafce5e6d2
Talis Lima <147412161+Thelozx@users.noreply.github.com>
2025-05-28 13:46:53 -0300

    feat: ajuste na pagina de armas e equipamentos q estavam em ingles

--- src/components/weapon-table.tsx
+++ src/components/weapon-table.tsx
@@ -12,39 +12,39 @@ export const WeaponTable = ({ title, weapons }: { title: string; weapons: Equipm
             <table className="w-full text-sm">
               <thead>
                 <tr className="bg-purple-900/30 text-gray-300">
-                  <th className="text-left p-3 border-r border-gray-600">Item</th>
-                  <th className="text-left p-3 border-r border-gray-600">Name</th>
-                  <th className="text-left p-3 border-r border-gray-600">Damage</th>
-                  <th className="text-left p-3 border-r border-gray-600">Attack Speed</th>
-                  <th className="text-left p-3 border-r border-gray-600">Range</th>
-                  <th className="text-left p-3 border-r border-gray-600">Defense</th>
-                  <th className="text-left p-3 border-r border-gray-600">Attributes</th>
-                  <th className="text-left p-3 border-r border-gray-600">Size</th>
-                  <th className="text-left p-3 border-r border-gray-600">Weight</th>
-                  <th className="text-left p-3 border-r border-gray-600">Drop by</th>
-                  <th className="text-left p-3 border-r border-gray-600">Buy From</th>
-                  <th className="text-left p-3">Sell To</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Item</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Nome</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Dano</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Velocidade de Ataque</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Alcance</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Defesa</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Atributos</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Tamanho</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Peso</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Drop Por</th>
+                  <th className="text-left p-4 border-r border-gray-600 font-semibold">Comprar De</th>
+                  <th className="text-left p-4 font-semibold">Vender Para</th>
                 </tr>
               </thead>
               <tbody>
                 {weapons.map((weapon, index) => (
                   <tr key={index} className="border-b border-gray-700 hover:bg-gray-700/30 transition-colors">
-                    <td className="p-3 border-r border-gray-600 text-center">
+                    <td className="p-4 border-r border-gray-600 text-center">
                       <div className="flex justify-center">
                         <WeaponBox title={weapon.name} imageUrl={weapon.imageUrl} />
                       </div>
                     </td>
-                    <td className="p-3 border-r border-gray-600 text-yellow-400 font-medium">{weapon.name}</td>
-                    <td className="p-3 border-r border-gray-600 text-green-400">{weapon.damage}</td>
-                    <td className="p-3 border-r border-gray-600 text-blue-400">{weapon.attackSpeed}</td>
-                    <td className="p-3 border-r border-gray-600 text-orange-400">{weapon.range}</td>
-                    <td className="p-3 border-r border-gray-600 text-purple-400">{weapon.defense}</td>
-                    <td className="p-3 border-r border-gray-600 text-gray-300">{weapon.attributes}</td>
-                    <td className="p-3 border-r border-gray-600 text-gray-300">{weapon.size}</td>
-                    <td className="p-3 border-r border-gray-600 text-gray-300">{weapon.weight}</td>
-                    <td className="p-3 border-r border-gray-600 text-red-400">{weapon.dropBy}</td>
-                    <td className="p-3 border-r border-gray-600 text-cyan-400">{weapon.buyFrom}</td>
-                    <td className="p-3 text-green-300">{weapon.sellTo}</td>
+                    <td className="p-4 border-r border-gray-600 text-yellow-400 font-medium">{weapon.name}</td>
+                    <td className="p-4 border-r border-gray-600 text-green-400">{weapon.damage}</td>
+                    <td className="p-4 border-r border-gray-600 text-blue-400">{weapon.attackSpeed}</td>
+                    <td className="p-4 border-r border-gray-600 text-orange-400">{weapon.range}</td>
+                    <td className="p-4 border-r border-gray-600 text-purple-400">{weapon.defense}</td>
+                    <td className="p-4 border-r border-gray-600 text-gray-300">{weapon.attributes}</td>
+                    <td className="p-4 border-r border-gray-600 text-gray-300">{weapon.size}</td>
+                    <td className="p-4 border-r border-gray-600 text-gray-300">{weapon.weight}</td>
+                    <td className="p-4 border-r border-gray-600 text-red-400">{weapon.dropBy}</td>
+                    <td className="p-4 border-r border-gray-600 text-cyan-400">{weapon.buyFrom}</td>
+                    <td className="p-4 text-green-300">{weapon.sellTo}</td>
                   </tr>
                 ))}
               </tbody>
