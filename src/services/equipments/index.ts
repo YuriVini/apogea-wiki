f620cc545aecc7ee1c9453a21a9c612c3b7ad212
Talis Lima <147412161+Thelozx@users.noreply.github.com>
2025-06-03 00:37:23 -0300

    feat: ajuste no index de equipments

--- src/services/equipments/index.ts
+++ src/services/equipments/index.ts
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query'
- 
+import { useQuery, useMutation } from '@tanstack/react-query'
+import { Api } from '../../@api/axios'
 import { ApiNoAuth } from '../../@api/axios'
 
 export const EQUIPMENTS_QUERY_KEY = 'equipments'
@@ -8,6 +8,33 @@ const fetchEquipments = async (): Promise<EquipmentsApiTypes.Equipment[]> => {
   const { data } = await ApiNoAuth.get('/equipments')
   return data
 }
+
+  export const useUpdateEquipment = () => {
+  return useMutation({
+    mutationFn: async (equipment: EquipmentsApiTypes.Equipment): Promise<EquipmentsApiTypes.Equipment> => {
+      const { data } = await Api.put<EquipmentsApiTypes.Equipment>(`/equipments/${equipment.id}`, equipment)
+      return data
+    },
+  })
+}
+
+export const useCreateEquipment = () => {
+  return useMutation({
+    mutationFn: async (equipment: Omit<EquipmentsApiTypes.Equipment, 'id'>): Promise<{ equipmentId: string }> => {
+      const { data } = await Api.post<{ equipmentId: string }>(`/equipments`, equipment)
+      return data
+    },
+  })
+}
+
+export const useDeleteEquipment = () => {
+  return useMutation({
+    mutationFn: async (equipmentId: string) => {
+      await Api.delete(`/equipments/${equipmentId}`)
+    },
+  })
+}
+
 
 export const useEquipments = () => {
   return useQuery({
