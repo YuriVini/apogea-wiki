a5a3d9603b77e0c70b30cd4467a8a6293b19fd75
YuriVini <yuri.ybgt.yfo@gmail.com>
2025-06-13 10:53:27 -0300

    fix: sort bug when show all weapons

--- src/components/skill-slot.tsx
+++ src/components/skill-slot.tsx
@@ -1,36 +1,45 @@
-import { useState } from 'react'
-import { SkillSelector } from './skill-selector'
-import { useBuilder } from '../context/builder'
+import { useState } from "react";
+import { SkillSelector } from "./skill-selector";
+import { useBuilder } from "../context/builder";
 
 interface SkillSlotProps {
-  type: EquipmentsApiTypes.SlotType
-  size?: 'normal' | 'small'
-  equipment?: EquipmentsApiTypes.Equipment
-  category?: EquipmentsApiTypes.CategoryType
-  slot?: EquipmentsApiTypes.CategoryType
+  type: EquipmentsApiTypes.SlotType;
+  size?: "normal" | "small";
+  equipment?: EquipmentsApiTypes.Equipment;
+  category?: EquipmentsApiTypes.CategoryType;
+  slot?: EquipmentsApiTypes.CategoryType;
 }
 
-export const SkillSlot = ({ type = 'weapon', category = 'chest', size = 'normal', equipment, slot }: SkillSlotProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const { isEditing } = useBuilder()
+export const SkillSlot = ({
+  type = "weapon",
+  category = "chest",
+  size = "normal",
+  equipment,
+  slot,
+}: SkillSlotProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { isEditing } = useBuilder();
 
-  const sizeClasses = size === 'small' ? 'w-8 h-8' : 'w-16 h-16'
+  const sizeClasses = size === "small" ? "w-8 h-8" : "w-16 h-16";
 
   const slotLabel = category
-    ? category === 'weapon-staff-bow-dagger-shield-glove'
-      ? category?.split('-')?.join(' ')
+    ? category === "sword-staff-bow-dagger-shield-glove"
+      ? category?.split("-")?.join(" ")
       : `${category.charAt(0).toUpperCase() + category.slice(1)} Slot`
-    : 'Equipment Slot'
+    : "Equipment Slot";
 
-  const categoryToSort = category === 'weapon-staff-bow-dagger-shield-glove' ? (category?.split('-') as EquipmentsApiTypes.CategoryType[]) : [category]
+  const categoryToSort =
+    category === "sword-staff-bow-dagger-shield-glove"
+      ? (category?.split("-") as EquipmentsApiTypes.CategoryType[])
+      : [category];
 
-  const imageUrl = equipment?.imageUrl
+  const imageUrl = equipment?.imageUrl;
 
   const handlePressSlot = () => {
     if (isEditing) {
-      setIsModalOpen(true)
+      setIsModalOpen(true);
     }
-  }
+  };
 
   return (
     <>
@@ -52,12 +61,20 @@ export const SkillSlot = ({ type = 'weapon', category = 'chest', size = 'normal'
         `}
       >
         <div>
-          <img src={imageUrl} alt={equipment?.name} className='h-10 w-10' />
+          <img src={imageUrl} alt={equipment?.name} className="h-10 w-10" />
         </div>
-        <div className='absolute -top-8 scale-0 group-hover:scale-100 transition-transform bg-gray-900 text-xs text-gray-300 px-2 py-1 rounded whitespace-nowrap'>{slotLabel}</div>
+        <div className="absolute -top-8 scale-0 group-hover:scale-100 transition-transform bg-gray-900 text-xs text-gray-300 px-2 py-1 rounded whitespace-nowrap">
+          {slotLabel}
+        </div>
       </button>
 
-      <SkillSelector isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} slotType={slot || category} type={type} category={categoryToSort} />
+      <SkillSelector
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        slotType={slot || category}
+        type={type}
+        category={categoryToSort}
+      />
     </>
-  )
-}
+  );
+};
